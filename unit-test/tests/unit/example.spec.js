23080d45ddc6e08facfb213259fe01e555be4008
joo97 <13295923180@163.com>
2022-02-12 17:44:05 +0800

    1. add .prettierrc.json 2. update example.spec.js

--- unit-test/tests/unit/example.spec.js
+++ unit-test/tests/unit/example.spec.js
@@ -1,12 +1,27 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount, mount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg },
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+describe('counter', () => {
+  const wrapper = mount(HelloWorld)
+  it('has a button', () => {
+    expect(wrapper.find('.counter').exists()).toBe(true)
+  })
+})
+
+describe('button:click', () => {
+  it('click btn increments counter by 1', async () => {
+    const wrapper = mount(HelloWorld)
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.vm.counter).toBe(1)
+  })
+})
